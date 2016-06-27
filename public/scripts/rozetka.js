daacb9a156472b44ef2ac334a9533dbfb85ce72c
Nata Entali <entalila@icloud.com>
2016-06-27 13:36:36 +0300

    Vas bi viebat

--- public/scripts/rozetka.js
+++ public/scripts/rozetka.js
@@ -3,12 +3,13 @@ var data = [
     id: 1,
     imgSrc: 'http://i1.rozetka.ua/goods/1479071/apple_macbook_air_13_mjve2uaa_images_1479071905.jpg',
     name: 'Apple MacBook Air 13" (MJVE2UA/A) + сертификат Розетка 1000грн в подарок!',
-    isMany: true,
+    isMany: 12,
     isCommented: false,
     comments: 47,
     isAvailable: true,
     memory: '4 ГБ',
     hdd: '128 ГБ',
+    hot: true,
     description: 'Экран 13.3" (1440x900) WXGA+ LED, глянцевый / Intel Core i5 (1.6 - 2.7 ГГц) / RAM 4 ГБ / SSD 128 ГБ / Intel HD Graphics 6000 / без ОД / Wi-Fi / Bluetooth / веб-камера / OS X Yosemite / 1.35 кг'
   },
 
@@ -16,12 +17,13 @@ var data = [
     id: 2,
     imgSrc: 'http://i1.rozetka.ua/goods/1628062/9502875_images_1628062867.jpg',
     name: 'Apple MacBook Air NEW 2015 (MJVE2)',
-    isMany: true,
+    isMany: 6,
     isCommented: true,
     comments: 0,
     isAvailable: true,
     memory: 4,
     hdd: 128,
+    hot: true,
     description: 'Экран 13.3" (1440x900) WXGA+ LED, глянцевый / Intel Core i5 (1.6 - 2.7 ГГц) / RAM 4 ГБ / SSD 128 ГБ / Intel HD Graphics 6000 / без ОД / Wi-Fi / Bluetooth / веб-камера / OS X Yosemite / 1.35 кг'
   },
 
@@ -29,27 +31,39 @@ var data = [
     id: 3,
     imgSrc: 'http://i1.rozetka.ua/goods/1479133/apple_macbook_pro_retina_13_mf839uaa_images_1479133967.jpg',
     name: 'Apple MacBook Pro Retina 13" (MF839UA/A)',
-    isMany: false,
+    isMany: 0,
     isCommented: false,
     comments: 18,
     isAvailable: false,
     memory: 8,
     hdd: 128,
+    hot: false,
     description: 'Экран 13.3" IPS (2560x1600) Retina LED, глянцевый / Intel Core i5 (2.7 ГГц) / RAM 8 ГБ / SSD 128 ГБ / Intel Iris Graphics 6100 / без ОД / Wi-Fi / Bluetooth / веб-камера / OS X Yosemite / 1.58 кг'
   }
 ];
 
+
+
 var ItemList = React.createClass({
   render: function() {
-    var dataInfo = this.props.arr.map(function(el) {
+    var list = this.props.arr.map(function(el) {
+
       return (
-        <Item comments={el.comments} imgSrc={el.imgSrc} name={el.name} memory={el.memory} hdd={el.hdd} description={el.description} key={el.id}/>
+        <Item comments={el.comments}
+              imgSrc={el.imgSrc}
+              name={el.name}
+              memory={el.memory}
+              hdd={el.hdd}
+              description={el.description}
+              isMany={el.isMany}
+              hot={el.hot}
+              key={el.id}/>
       );
     });
 
     return (
       <div className="item__list">
-        {dataInfo}
+        {list}
       </div>
     )
   }
@@ -59,14 +73,16 @@ var Item = React.createClass ({
   render: function() {
 
     return (
-
       <div className="item">
         <img src={this.props.imgSrc} className="item__img"/>
         <p className="item__id">{this.props.id} </p>
         <p className="item__name">{this.props.name} </p>
-        <IsMany />
-        <Btn />
-        <BtnDis />
+
+
+        {this.props.isMany < 10 ? <IsMany amount={this.props.isMany} /> : null}
+
+        <Btn color={this.props.hot}/>
+
         <span className="item__comments">{this.props.comments} отзывов</span>
         <p className="item__memory">{this.props.memory} </p>
         <p className="item__hdd">{this.props.hdd} </p>
@@ -79,51 +95,32 @@ var Item = React.createClass ({
 
 var Btn = React.createClass ({
   render: function() {
+    var cls = 'btn ';
+
     return (
-      <div className="item__btn">
-        <button className="btn btn_green" >Купить</button>
-      </div>
+      <button className={this.props.color ? cls : cls + 'is-green'}>
+        Купить
+      </button>
     );
   }
 });
-
-var BtnDis = React.createClass ({
-  render: function() {
-    return (
-      <div className="item__btn">
-        <button className="btn btn_white">Сообщите, когда появится</button>
-      </div>
-    );
-  }
-});
-
-//var Comments = React.createClass ({
-//  render: function() {
-//    var comment = data;
-//    var commentInfo = comment.map(function(el) {
-//      return (
-//
-//      );
-//    });
-//
-//    return (
-//      <div className="item__list">
-//        {commentInfo}
-//      </div>
-//    )
-//  }
-//});
 
 var IsMany = React.createClass ({
   render: function () {
+
     return (
-      <div>
-        <p className="orange"><strong>Заканчивается</strong></p>
-        <p className="black"><strong>Нет в наличии</strong></p>
-      </div>
+        <div className="is-many">
+          {!this.props.amount ?
+          <p className="black"><strong>Нет в наличии</strong></p>
+          :
+          <p className="orange"><strong>Заканчивается</strong></p>
+          }
+        </div>
     );
   }
 });
+
+
 
 ReactDOM.render(
   <ItemList arr={data}/>,
